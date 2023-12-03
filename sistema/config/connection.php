d9f684d3b1ba67ea97c9afacba27ee2028869882
Gabriel Carvalho <gabriel.carvalho.pg@gmail.com>
2023-12-03 12:21:24 -0300

    roletando petshops

--- sistema/config/connection.php
+++ sistema/config/connection.php
@@ -1,12 +1,12 @@
 <?php
-// $servername = "localhost:3306";
-// $username = "ezpets_admin";
-// $password = "ezpets@admin";
-// $dbname = "ezpets_db";
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "localhost:3306";
+$username = "ezpets_admin";
+$password = "ezpets@admin";
 $dbname = "ezpets_db";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "ezpets_db";
 
 try {
 $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
